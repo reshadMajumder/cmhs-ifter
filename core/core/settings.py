9459f743a344520a066fbc2d552d22a04e283c0e
reshadMajumder <reshad22205341197@diu.edu.bd>
2026-02-27 15:51:28 +0600

    added csrf raillway

--- core/core/settings.py
+++ core/core/settings.py
@@ -306,6 +306,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "https://cmhs-ifter.vercel.app",
     "https://iftar.cmhsians.com",
+    "https://cmhs-ifter-production.up.railway.app",
     
 
 ]
